c8605a18cfefc3deef32e62d8a256dff91cd518b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-09-09 04:36:37 -0700

    add midi slider

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -5,10 +5,11 @@
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
         : AudioProcessorEditor (&p), processorRef (p)
 {
-    juce::ignoreUnused (processorRef);
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    initMidiVolumeSlider();
+
+
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -23,11 +24,26 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+
+    g.drawFittedText ("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    // subcomponents in your editor...
+
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
+}
+
+void AudioPluginAudioProcessorEditor::initMidiVolumeSlider()
+{
+    midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled(true, false, this);
+    midiVolume.setTextValueSuffix(" Volume");
+    midiVolume.setValue(1.0);
+
+    addAndMakeVisible(&midiVolume);
 }
--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -13,10 +13,13 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void initMidiVolumeSlider();
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
+
+    juce::Slider midiVolume;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
